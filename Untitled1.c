707ff959550ad02af1ceccd2d7377694b5ad3e7d
unknown <adrien.lalisse18@gmail.com>
2017-10-09 21:44:38 +0200

    first commit

---
+++ Untitled1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int resultat,a,b;
+    printf("a= ");
+    scanf("%d", &a);
+    printf("b= ");
+    scanf("%d", &b);
+    resultat = a+b;
+    printf("%d + %d = %d\n", a, b, resultat);
+
+return 0;
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int valeur1,valeur2,resultat;
+    printf("quelle est la premiere valeur ?");
+    scanf("%d", &valeur1);
+    printf("quelle est la deuxieme valeur ?");
+    scanf("%d", &valeur2);
+return 0;
+}
